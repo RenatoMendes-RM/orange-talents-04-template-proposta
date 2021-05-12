4abd5dc07cbd1dd3881f9bbc2d264fe0b575dc0c
RenatoMendes-RM <renatomendes.rm@gmail.com>
2021-05-12 10:17:24 -0300

    setup do projeto

---
+++ src/main/java/br/com/ot4rmsproposta/propostaot4rms/PropostaOt4RmsApplication.java
@@ -0,0 +1,13 @@
+package br.com.ot4rmsproposta.propostaot4rms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropostaOt4RmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropostaOt4RmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ot4rmsproposta/propostaot4rms/PropostaOt4RmsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ot4rmsproposta.propostaot4rms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaOt4RmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
