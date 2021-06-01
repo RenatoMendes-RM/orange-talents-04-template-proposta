68c6798e1e06f7f51bea183d51390f70dff8cf18
RenatoMendes-RM <renatomendes.rm@gmail.com>
2021-06-01 08:34:20 -0300

    040 Rodar aplicacao no docker

--- src/main/java/br/com/ot4rmsproposta/propostaot4rms/novaProposta/AnaliseSolicitacaoClient.java
+++ src/main/java/br/com/ot4rmsproposta/propostaot4rms/novaProposta/AnaliseSolicitacaoClient.java
@@ -3,7 +3,7 @@ package br.com.ot4rmsproposta.propostaot4rms.novaProposta;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "analise-solicitacao", url = "http://localhost:9999")
+@FeignClient(name = "analise-solicitacao", url = "http://analise:9999")
 public interface AnaliseSolicitacaoClient {
 
     @PostMapping("/api/solicitacao")
--- src/main/java/br/com/ot4rmsproposta/propostaot4rms/novaProposta/AssociaCartaoPropostaClient.java
+++ src/main/java/br/com/ot4rmsproposta/propostaot4rms/novaProposta/AssociaCartaoPropostaClient.java
@@ -4,7 +4,7 @@ import br.com.ot4rmsproposta.propostaot4rms.Cartao.CartaoResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "associa-cartao", url = "http://localhost:8888")
+@FeignClient(name = "associa-cartao", url = "http://contas:8888")
 public interface AssociaCartaoPropostaClient {
 
     @GetMapping("/api/cartoes")
